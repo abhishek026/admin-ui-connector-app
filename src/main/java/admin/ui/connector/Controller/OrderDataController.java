89daf06c25901218d88e291488aa9d4d87364630
Abhishek Kumar <abhishek@192.168.1.46>
2025-02-09 10:14:12 +0530

    add full screen

--- src/main/java/admin/ui/connector/Controller/OrderDataController.java
+++ src/main/java/admin/ui/connector/Controller/OrderDataController.java
@@ -20,79 +20,78 @@ import java.util.Map;
 @RequestMapping("/api")
 public class OrderDataController {
 
-    @Autowired
-    private OrderDataService orderDataService;
+	@Autowired
+	private OrderDataService orderDataService;
 
-    @Autowired
-    private KiteClientBusiness kiteClientBusiness;
+	@Autowired
+	private KiteClientBusiness kiteClientBusiness;
 
+	@GetMapping("/expiry-dates")
+	public ResponseEntity<?> getExpiryDates() {
+		Map<String, String> expiryDates = orderDataService.getExpiryDates();
+		return ResponseUtil.buildResponse(expiryDates);
+	}
 
-    @GetMapping("/expiry-dates")
-    public ResponseEntity<?> getExpiryDates() {
-        Map<String, String> expiryDates = orderDataService.getExpiryDates();
-        return ResponseUtil.buildResponse(expiryDates);
-    }
+	@GetMapping("/expiry-data")
+	public ResponseEntity<?> getOrderData(@RequestParam("expiry") String expiry) {
+		OrderPlacement orderPlacement = orderDataService.getOrderDataByExpiry(expiry);
+		return ResponseUtil.buildResponse(orderPlacement);
+	}
 
-    @GetMapping("/expiry-data")
-    public ResponseEntity<?> getOrderData(@RequestParam("expiry") String expiry) {
-        OrderPlacement orderPlacement = orderDataService.getOrderDataByExpiry(expiry);
-        return ResponseUtil.buildResponse(orderPlacement);
-    }
+	@PostMapping("/save-order-template")
+	public ResponseEntity<?> saveOrderTemplate(@RequestBody List<OrderTemplate> orderTemplates) {
+		orderDataService.saveOrderTemplate(orderTemplates);
+		return ResponseUtil.buildResponse(HttpStatus.CREATED, "Template(s) created successfully!");
+	}
 
-    @PostMapping("/save-order-template")
-    public ResponseEntity<?> saveOrderTemplate(@RequestBody List<OrderTemplate> orderTemplates) {
-        orderDataService.saveOrderTemplate(orderTemplates);
-        return ResponseUtil.buildResponse(HttpStatus.CREATED,"Template(s) created successfully!");
-    }
+	@PatchMapping("/update-order-template")
+	public ResponseEntity<?> updateOrderTemplate(@RequestBody List<OrderTemplate> orderTemplates) {
+		try {
+			orderDataService.updateOrderTemplates(orderTemplates);
+			return ResponseUtil.buildResponse("Template(s) modified successfully!");
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + e.getMessage());
+		}
+	}
 
-    @PatchMapping("/update-order-template")
-    public ResponseEntity<?> updateOrderTemplate(@RequestBody List<OrderTemplate> orderTemplates) {
-        try {
-            orderDataService.updateOrderTemplates(orderTemplates);
-            return ResponseUtil.buildResponse("Template(s) modified successfully!");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Error: " + e.getMessage());
-        }
-    }
+	@GetMapping("/getAllTemplates")
+	public ResponseEntity<?> getAllTemplates() {
+		Map<String, List<OrderTemplate>> templates = orderDataService.getAllTemplates();
+		return ResponseUtil.buildResponse(templates);
+	}
 
-    @GetMapping("/getAllTemplates")
-    public ResponseEntity<?> getAllTemplates() {
-        Map<String, List<OrderTemplate>> templates = orderDataService.getAllTemplates();
-        return ResponseUtil.buildResponse(templates);
-    }
+	@GetMapping("/find-order-template")
+	public ResponseEntity<?> findTemplateByName(@RequestParam String templateName) {
+		Map<String, List<OrderTemplate>> templates = orderDataService.findTemplateByName(templateName);
+		return ResponseUtil.buildResponse(templates);
+	}
 
-    @GetMapping("/find-order-template")
-    public ResponseEntity<?> findTemplateByName(@RequestParam String templateName) {
-        Map<String, List<OrderTemplate>> templates = orderDataService.findTemplateByName(templateName);
-        return ResponseUtil.buildResponse(templates);
-    }
+	@DeleteMapping("/deleteByTemplateName")
+	public ResponseEntity<?> deleteByTemplateName(@RequestParam String templateName) {
+		try {
+			orderDataService.deleteByTemplateName(templateName);
+			return ResponseUtil.buildResponse("Template(s) with name '" + templateName + "' deleted successfully!");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Error deleting template(s): " + e.getMessage());
+		}
+	}
 
-    @DeleteMapping("/deleteByTemplateName")
-    public ResponseEntity<?> deleteByTemplateName(@RequestParam String templateName) {
-        try {
-            orderDataService.deleteByTemplateName(templateName);
-            return ResponseUtil.buildResponse("Template(s) with name '" + templateName + "' deleted successfully!");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error deleting template(s): " + e.getMessage());
-        }
-    }
+	@GetMapping("/get-broker-list")
+	public ResponseEntity<?> getBrokerList() {
+		List<Broker> brokers = orderDataService.getBrokerList();
+		return ResponseUtil.buildResponse(brokers);
+	}
 
-    @GetMapping("/get-broker-list")
-    public ResponseEntity<?> getBrokerList() {
-        List<Broker>  brokers= orderDataService.getBrokerList();
-        return ResponseUtil.buildResponse(brokers);
-    }
+	@PostMapping("/generateToken")
+	public ResponseEntity<?> generateToken(@RequestBody KiteCredentials credentials) {
+		kiteClientBusiness.generateTokens(credentials);
+		return ResponseUtil.buildResponse(HttpStatus.CREATED, "Token generated successfully!");
+	}
 
-    @PostMapping("/generateToken")
-    public ResponseEntity<?> generateToken(@RequestBody KiteCredentials credentials) {
-       kiteClientBusiness.generateTokens(credentials);
-        return ResponseUtil.buildResponse(HttpStatus.CREATED,"Token generated successfully!");
-    }
+	@GetMapping("/get-active-token-brokers")
+	public ResponseEntity<?> getActiveBrokers() {
+		return ResponseUtil.buildResponse(orderDataService.getActiveTokenBrokers());
 
-    @GetMapping("/get-active-token-brokers")
-    public List<Broker> getActiveBrokers() {
-        return orderDataService.getActiveTokenBrokers();
-    }
+	}
 }
